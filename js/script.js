e85b186e1747337ec812ffc8141a04a1f13a33d8
kayoko-matuski <nyaataco.contact@gmail.com>
2025-09-09 15:29:07 +0900

    animation効果を付けてみる

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("it's works!");
+document.addEventListener('DOMContentLoaded', () => {
+    const targets = document.querySelectorAll('h2.heading');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("fade-up");
+        } else {
+            entry.target.classList.remove("fade-up");
+        }
+        });
+    }, observerOptions);
+
+    targets.forEach((target) => {
+        observer.observe(target);
+    });
+});
