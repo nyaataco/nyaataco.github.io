060e53afe1468a151f7f10c92ae54ffb7cbe9d18
kayoko-matuski <nyaataco.contact@gmail.com>
2025-09-10 11:46:26 +0900

    animation追加しようと思ったけれど、読み込めなかった時に何も表示されなくなるので一旦無効に。一応コードは置いておく

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
     const targets = document.querySelectorAll('h2.heading');
+    // const inner = document.querySelector('#works .inner');
 
     const observerOptions = {
         root: null,
         rootMargin: "0px",
-        threshold: 0.1
+        threshold: 0.3
     };
 
-    const observer = new IntersectionObserver((entries, observer) => {
+    // fade up animation
+    const fadeUpObserver = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("fade-up");
-        } else {
-            entry.target.classList.remove("fade-up");
-        }
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fade-up");
+            } else {
+                entry.target.classList.remove("fade-up");
+            }
         });
     }, observerOptions);
 
+    // fade in animation
+    const fadeInObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fade-in");
+            } else {
+                entry.target.classList.remove("fade-in");
+            }
+        });
+    }, observerOptions);
+
+
+
     targets.forEach((target) => {
-        observer.observe(target);
+        fadeUpObserver.observe(target);
     });
+
+
+    // if (inner) {
+    //     fadeInObserver.observe(inner);
+    // }
+
 });
