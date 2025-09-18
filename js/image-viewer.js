6434dc9602c33f206529ebff9e03a23ddbe7f6e4
kayoko-matuski <nyaataco.contact@gmail.com>
2025-09-18 11:09:19 +0900

    image veiwerの基本機能を作成

--- js/image-viewer.js
+++ js/image-viewer.js
@@ -1,1 +1,13 @@
-console.log('読み込みおっけー');
+document.addEventListener('DOMContentLoaded', () => {
+    const imageElements = document.querySelectorAll(".image");
+    const DisplayErea = document.querySelector(".large-image");
+
+    imageElements.forEach(el => {
+        el.addEventListener("click", function() {
+            const dataName = this.dataset.name;
+            DisplayErea.className = "large-image"
+            DisplayErea.classList.add(dataName);
+        });
+    });
+
+});
