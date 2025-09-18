3a4188b36a6fbbc4510b6cf8fed969a0d7e0e8d5
kayoko-matuski <nyaataco.contact@gmail.com>
2025-09-18 11:44:05 +0900

    PCは画像スクロールして見れる

--- js/image-viewer.js
+++ js/image-viewer.js
@@ -5,8 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
     imageElements.forEach(el => {
         el.addEventListener("click", function() {
             const dataName = this.dataset.name;
-            DisplayErea.className = "large-image"
-            DisplayErea.classList.add(dataName);
+            const newImageSrc = `images/MoutainTailsCafe/${dataName}.jpg`;
+
+            const newImg = document.createElement("img");
+            newImg.src = newImageSrc;
+            newImg.alt = dataName;
+
+            const oldImage = DisplayErea.querySelector("img")
+            DisplayErea.replaceChild(newImg, oldImage);
         });
     });
 
